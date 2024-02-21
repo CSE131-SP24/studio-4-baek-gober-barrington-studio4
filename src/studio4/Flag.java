27fc7b9824ea0d9725c4d8a2fcbac77e56773079
justinbaek <justinbaek@localhost>
2024-02-21 14:15:43 -0600

    We made a really cool flag and made a lot of designs on the side. :)

--- src/studio4/Flag.java
+++ src/studio4/Flag.java
@@ -1,11 +1,22 @@
 package studio4;
 
 import java.awt.Color;
+import java.awt.Font;
 
 import edu.princeton.cs.introcs.StdDraw;
 
 public class Flag {
-	public static void main(String[] args) {
-		
+	public static void main(String[] args) 
+	{
+		StdDraw.setCanvasSize(600, 400);
+	       StdDraw.setScale(0, 20);
+	       StdDraw.setPenColor(255, 0, 0);
+	       StdDraw.filledRectangle(10, 10, 10, 10);
+	       StdDraw.setPenColor(StdDraw.YELLOW);
+	       Font freedom = new Font("Papyrus", Font.BOLD, 42);
+	    StdDraw.setFont(freedom);
+	       StdDraw.text(10, 10, "Profits are stolen wages");
+	       StdDraw.show();
+	     
 	}
 }
--- src/studio4/InterpretDrawingFile.java
+++ src/studio4/InterpretDrawingFile.java
@@ -1,5 +1,6 @@
 package studio4;
 
+import java.awt.Color;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
@@ -14,11 +15,37 @@ import edu.princeton.cs.introcs.StdDraw;
  */
 public class InterpretDrawingFile {
 
-	public static void main(String[] args) throws FileNotFoundException {
+	public static void main(String[] args) throws FileNotFoundException 
+	{
 		JFileChooser chooser = new JFileChooser("resources");
 		chooser.showOpenDialog(null);
 		File f = new File(chooser.getSelectedFile().getPath());
 		Scanner in = new Scanner(f); //making Scanner with a File
+		String shapeType = in.next();
+		int redComponent = in.nextInt();
+		int greenComponent = in.nextInt();
+		int blueComponent = in.nextInt();
+		boolean isFilled = in.nextBoolean();
+		double parameterOne = in.nextDouble();
+		double parameterTwo = in.nextDouble();
+		double parameterThree = in.nextDouble();
+		double parameterFour = in.nextDouble();
+		double parameterFive = in.nextDouble();
+		double parameterSix = in.nextDouble();
 		
+		double[] x = {parameterOne, parameterTwo, parameterThree};
+		double[] y = {parameterFour, parameterFive, parameterSix};
+		
+		
+		Color color = new Color(redComponent, greenComponent, blueComponent);
+		StdDraw.setPenColor(color);
+		if (isFilled) 
+		{
+			StdDraw.filledCircle(parameterOne, parameterTwo, parameterThree);
+		}
+		else
+		{
+			StdDraw.polygon(x, y);
+		}
 	}
 }
